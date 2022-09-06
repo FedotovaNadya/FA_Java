9cf41fd0fe1277604c67736121bb0a976bacc95d
Nadya_Fedotova <nadya.fedotova01@mail.ru>
2022-09-06 15:43:29 +0300

    rename

--- sem1_1/task1.java
+++ sem1_1/task1.java
@@ -1,4 +1,4 @@
-// Выведите на экран текст Hello World
+//Выведите на экран текст Hello World
 class task1 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
